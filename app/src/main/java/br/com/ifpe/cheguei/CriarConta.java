fe9c666aeea0e13ce7d0243d9019078b193087b4
jonasfp <jfp@discente.ifpe.edu.br>
2022-05-01 22:20:05 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/br/com/ifpe/cheguei/CriarConta.java
@@ -0,0 +1,14 @@
+package br.com.ifpe.cheguei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriarConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.criarconta);
+    }
+}
