b6f64af1d24157659b1ba96a1054f05c2fe3aa6d
jonasfp <jfp@discente.ifpe.edu.br>
2022-05-03 22:13:44 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/br/com/ifpe/cheguei/Encomendas.java
@@ -0,0 +1,14 @@
+package br.com.ifpe.cheguei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Encomendas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.encomendas);
+    }
+}
